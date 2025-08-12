e8b2237b6a3647d7238d70ca61e83392c5591a05
ToToKu <totokunda@gmail.com>
2025-08-12 17:02:41 -0600

    Remove names

--- tests/quantize/test_transformer_quantize.py
+++ tests/quantize/test_transformer_quantize.py
@@ -2,7 +2,7 @@ from src.quantize import TransformerQuantizer
 
 quantizer = TransformerQuantizer(
     output_path="./wan_1.3b.gguf",
-    model_path='/Users/tosinkuye/apex-diffusion/components/Wan-AI_Wan2.1-T2V-1.3B-Diffusers/transformer',
+    model_path='apex-diffusion/components/Wan-AI_Wan2.1-T2V-1.3B-Diffusers/transformer',
     architecture="wan",
     quantization="Q8_0",
 )
