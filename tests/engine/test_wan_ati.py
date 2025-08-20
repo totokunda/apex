40a4b861c7a24748efcaef3335fc17f6e050f8f6
ToToKu <totokunda@gmail.com>
2025-08-20 08:29:06 +0000

    Add support for Wan ATI and Wan InifiniteTalk

--- tests/engine/test_wan_ati.py
+++ tests/engine/test_wan_ati.py
@@ -8,8 +8,8 @@ engine = create_engine("wan", "manifest/wan/wan_ati_14b.yml", "ati", attention_t
 
 prompt = "A brown bear lying in the shade beside a rock, resting on a bed of grass."
 negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
-image = '/home/tosinkuye/apex/ATI/examples/images/bear.jpg'
-trajectory = '/home/tosinkuye/apex/ATI/examples/tracks/bear.pth'
+image = 'assets/image/bear.png'
+trajectory = 'assets/wan/bear.pth'
     
 video = engine.run(
     image=image,
